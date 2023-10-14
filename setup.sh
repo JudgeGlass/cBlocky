c06c1d8e4533ab6d46ba545430041d776945ca35
Hunter Wilcox <hunter.wilcox@judgeglass.net>
2023-10-14 11:45:19 -0500

    Setup script

---
+++ setup.sh
@@ -0,0 +1,22 @@
+!#/bin/bash
+
+sudo apt update
+sudo apt install git libsdl2-dev libglew-dev -y
+
+wget https://raw.githubusercontent.com/nothings/stb/master/stb_image.h
+sudo mv stb_image.h /usr/local/include
+wget https://raw.githubusercontent.com/nothings/stb/master/stb_ds.h
+sudo mv stb_ds.h /usr/local/include
+
+git clone https://github.com/recp/cglm
+cd cglm
+mkdir build && cd build
+cmake ..
+make
+sudo make install
+
+cd ../..
+rm -rf cglm
+mkdir build && cd build
+cmake ..
+make
